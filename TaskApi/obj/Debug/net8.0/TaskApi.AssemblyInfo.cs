214d7d37ad6a4acc8fbeec12abf1da5f8c5e73ca
AMOGH MS <amoghms@AMOGHs-MacBook-Air.local>
2026-04-06 16:02:13 +0530

    fix workflow

--- TaskApi/obj/Debug/net8.0/TaskApi.AssemblyInfo.cs
+++ TaskApi/obj/Debug/net8.0/TaskApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TaskApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f730e85d13eae65bf0549ac0c2dde2c8f1beb8d1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+63b6d498096ef28fa347b8729cb6645b32b10631")]
 [assembly: System.Reflection.AssemblyProductAttribute("TaskApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TaskApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
